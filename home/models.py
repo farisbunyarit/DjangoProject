9ddc26f9be5cb997485cf863d5de249ee254ea83
Faris Bunyarit <faris.bunyarit1@gmail.com>
2026-09-13 14:42:57 +0700

    Add product stock

--- home/models.py
+++ home/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.URLField(max_length=500, blank=True)
     badge = models.CharField(max_length=50, blank=True)
